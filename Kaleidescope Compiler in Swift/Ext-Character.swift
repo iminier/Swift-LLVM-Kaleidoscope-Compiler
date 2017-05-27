a36cb78873c4dcdb5e924da86a56014206d48b0a
Ivan Minier <ivanminier@me.com>
2017-05-27 09:36:35 -0700

    Character extension added

--- Kaleidescope Compiler in Swift/Ext-Character.swift
+++ Kaleidescope Compiler in Swift/Ext-Character.swift
@@ -5,10 +5,16 @@
 //  Created by Ivan Minier on 5/27/17.
 //  Copyright © 2017 Ivan Minier. All rights reserved.
 //
+import Darwin
 
 extension Character {
     
     var value: Int32 {
         return Int32(String(self).unicodeScalars.first!.value)
     }
+    
+    var isSpace: Bool {
+        return isalnum(value) != 0 || self == "_"
+    }
+    
 }
