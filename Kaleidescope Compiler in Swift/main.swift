36708fc2f26ac018eb58e1c16e835763e5381824
Ivan Minier <ivanminier@me.com>
2017-05-27 09:21:05 -0700

    Initial Commit

---
+++ Kaleidescope Compiler in Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Kaleidescope Compiler in Swift
+//
+//  Created by Ivan Minier on 5/27/17.
+//  Copyright © 2017 Ivan Minier. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
